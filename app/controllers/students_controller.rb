de09ce7b6465c3792586daac4acb3cbb36eb0cc9
Andrew Stahl <andrewstahl96@gmail.com>
2022-12-14 19:45:21 -0500

    commit tree 0d200bdfb844ea9377321da556e404ac6489d113

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,18 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    # byebug
+    if params[:name] != nil
+      students = Student.where("first_name like ?", params[:name]).or(Student.where("last_name like ?", params[:name]))
+    else
+      students = Student.all
+    end
     render json: students
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
